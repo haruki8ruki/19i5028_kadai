48bbf642cdc836a3fe28652a10b5106945674a8c
haruki8ruki <82562257+haruki8ruki@users.noreply.github.com>
2021-06-09 00:56:37 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        sprite.move(1)
+    }
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(0, 0)
+basic.forever(function () {
+	
+})
