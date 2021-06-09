c3a5ab71dbb67818c0a3ed195fd62dcf1e9ce8de
haruki8ruki <82562257+haruki8ruki@users.noreply.github.com>
2021-06-09 18:09:58 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,36 @@
-input.onButtonPressed(Button.A, function () {
+input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
-        sprite.move(1)
+        led.toggle(0, 2)
+        led.toggle(4, 2)
+        basic.pause(100)
+        led.toggle(0, 3)
+        led.toggle(4, 3)
     }
 })
-let sprite: game.LedSprite = null
-sprite = game.createSprite(0, 0)
-basic.forever(function () {
-	
+input.onGesture(Gesture.FreeFall, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    basic.showLeds(`
+        . # . # .
+        . . # . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    for (let index = 0; index < 4; index++) {
+        led.toggle(0, 4)
+        led.toggle(4, 4)
+        basic.pause(100)
+        led.toggle(0, 3)
+        led.toggle(4, 3)
+    }
 })
+led.plot(1, 0)
+led.plot(3, 0)
+led.plot(2, 1)
+led.plot(1, 3)
+led.plot(2, 3)
+led.plot(3, 3)
